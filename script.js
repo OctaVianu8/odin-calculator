25a4e22d40284476dd1b7371fbe376ef08e4b02d
Stanescu Octavian <stanescu.matei.octavian@gmail.com>
2022-08-04 16:19:07 +0300

    add calculator buttons

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("Hello world!")
+
+let buttonStrings = [
+    'C','()','%','/',
+    '7','8','9','*',
+    '4','5','6','-',
+    '1','2','3','+',
+    '+/-','0','.','=',
+]
+
+generateButtons()
+
+function generateButtons()
+{
+    buttonStrings.forEach(string => {
+        let currButton = document.createElement('button');
+        currButton.textContent = string;
+        currButton.className = 'calculator-button';
+        document.querySelector('.buttons-container').appendChild(currButton);
+    })
+}
